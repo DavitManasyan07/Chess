919a099d5b6866b572c2a4747097f83b1c1c12b8
Davit Manasyan <davit.manasyan.07@gmail.com>
2024-05-24 21:02:46 +0400

    chess upd 1.0

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-console.log("cadscdscdsc");
-console.log(
-  "wncjdsncjsndcjkndsjcnsjkcnsdjcnjsdnjdnchsdbcjndssndjkcnsdkjcndkjcnsdjkncjk"
-);
+const board = document.getElementById("board");
+const table = document.createElement("table")
+board.append(table)
+for(let y = 0;y < 8;y++){
+const trElement = document.createElement("tr")
+    for(let x = 0;x < 8;x++){
+        const tdElement = document.createElement("td")
+        if((x+y)%2 === 0){
+            tdElement.classList.add("sev")
+        }else{
+            tdElement.classList.add("spitak")
+        }
+        trElement.append(tdElement)
+    }
+    table.append(trElement);
+}
