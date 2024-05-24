37bf6bf88b61bc925cb77e89408c653d8feaa321
Davit Manasyan <davit.manasyan.07@gmail.com>
2024-05-24 18:45:00 +0400

    7 commit

--- script.js
+++ script.js
@@ -2,11 +2,3 @@ console.log("cadscdscdsc");
 console.log(
   "wncjdsncjsndcjkndsjcnsjkcnsdjcnjsdnjdnchsdbcjndssndjkcnsdkjcndkjcnsdjkncjk"
 );
-for (let i = 0; i < 10; i++) {
-  console.log("alahakbar");
-}
-
-
-console.log("nixuya sebe");
-
-console.log("blaaaaaaa")
