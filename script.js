0db1e0d91dbbd128d4ac655028ffacec49be6e54
Davit Manasyan <davit.manasyan.07@gmail.com>
2024-05-30 12:52:56 +0400

    upd 1

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const board = document.getElementById("board");
-const table = document.getElementById("tableBoard")
+const table = document.getElementById("tableBoard");
 
-for(let y = 0;y < 8;y++){
-const trElement = document.createElement("tr")
-    for(let x = 0;x < 8;x++){
-        const tdElement = document.createElement("td")
-        if((x+y)%2 === 0){
-            tdElement.classList.add("sev")
-        }else{
-            tdElement.classList.add("spitak")
-        }
-        trElement.append(tdElement)
+for (let y = 0; y < 8; y++) {
+  const trElement = document.createElement("tr");
+  for (let x = 0; x < 8; x++) {
+    const tdElement = document.createElement("td");
+    if ((x + y) % 2 === 0) {
+      tdElement.classList.add("sev");
+    } else {
+      tdElement.classList.add("spitak");
     }
-    table.append(trElement);
+    trElement.append(tdElement);
+  }
+  table.append(trElement);
 }
